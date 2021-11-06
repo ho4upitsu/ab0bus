7ce6cd6779eb1150bb20ccf8df5e9407d8db31ac
ho4upitsu <90406803+ho4upitsu@users.noreply.github.com>
2021-11-06 18:17:06 +0200

    lab2

---
+++ oop_lab_2/oop_lab_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace oop_lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введіть n - ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double k = Math.Pow(n,2), i = 0;
+            double a = 0D;
+            
+            while (n*k != k)
+            {
+                
+                a += (Math.Pow(k, 2) + Math.Pow(-1,k-1) * 2 * k - 1)/(Math.Pow(k,2)+2);
+                Console.WriteLine("a[" + i + "] = " + a);
+                i++;
+            }
+            
+        }
+    }
+}
